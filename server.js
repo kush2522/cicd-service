7ebbff70333a1ec98532b45f8fc02f25f712d283
kush2522 <70235232+kush2522@users.noreply.github.com>
2021-03-19 02:25:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1>Kubernetes is awesome.</h1>");
+  response.end("<h1>Just getting Started.</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);

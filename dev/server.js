cc1e001266b4c976bc1f71e42a0c187a340d7047
Kush Batra <er.kushbatra@gmail.com>
2021-03-19 17:04:39 +0530

    Removing dev

--- dev/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var handleRequest = function(request, response) {
-  response.writeHead(200);
-  response.end("<h1>Test is successful with cloud run!!</h1>");
-}
-var www = http.createServer(handleRequest);
-www.listen(8080);
